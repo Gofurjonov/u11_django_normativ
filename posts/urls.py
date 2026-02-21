bd5d586c885465c45cf9979df54c4af1475c9b11
Davronbek <gafurjonovdavronbek@gmail.com>
2026-02-21 15:42:30 +0500

    4-normativ: Search va Pagination qo'shildi

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('deleted/', views.deleted_posts, name='deleted_posts'),
     path('create/', views.post_create, name='post_create'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
     path('<int:pk>/edit/', views.post_update, name='post_update'),
     path('<int:pk>/delete/', views.post_delete, name='post_delete'),
     path('<int:pk>/restore/', views.post_restore, name='post_restore'),
--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts_list = Post.objects.all()
+    search_query = request.GET.get('q', '')
+
+    if search_query:
+        posts_list = posts_list.filter(
+            Q(title__icontains=search_query) |
+            Q(content__icontains=search_query)
+        )
+
+    paginator = Paginator(posts_list, 5)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.get_page(page_number)
+
+    context = {
+        'posts': posts,
+        'search_query': search_query,
+    }
+
     return render(request, 'posts/post_list.html', {'posts': posts})
 
 def deleted_posts(request):
@@ -58,3 +77,7 @@ def post_hard_delete(request, pk):
         post.hard_delete()
         return redirect('deleted_posts')
     return render(request, 'posts/post_confirm_hard_delete.html', {'post': post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post.all_objects, pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
